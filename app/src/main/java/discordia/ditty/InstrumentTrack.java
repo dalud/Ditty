cfe2c3d1c71ea605ec40d0d1e2e3c40467bc6350
dalud <jaakko.niska@gmail.com>
2016-09-13 17:39:02 +0300

    ....

--- app/src/main/java/discordia/ditty/InstrumentTrack.java
+++ app/src/main/java/discordia/ditty/InstrumentTrack.java
@@ -5,21 +5,27 @@ import android.media.MediaPlayer;
 /**
  * Created by dalud on 10.9.2016.
  */
-public class InstrumentTrack {
+public class InstrumentTrack extends Thread{
 
     int[] pattern;
     MediaPlayer mp;
+    int step;
 
-    public InstrumentTrack(int[] pattern, MediaPlayer mp) {
-
+    public InstrumentTrack(int[] pattern, MediaPlayer mp, int step) {
+        this.step = step;
         this.pattern = pattern;
         this.mp = mp;
     }
 
-    public void run(int i) {
-        if(pattern[i] != 0) {
+    public void run() {
+        while(true) {
             if (mp.isPlaying()) mp.seekTo(0);
             else mp.start();
+            try {
+                sleep(step);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
